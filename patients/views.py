b43ea6a2aa50c3fbb831631f7f1d8f0d144b6369
yakatyansh <ykpetrolhead08@outlook.com>
2026-05-14 17:39:02 +0530

    add protection and fliter logic

--- patients/views.py
+++ patients/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 from .models import Patient
 from .serializers import PatientSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class PatientViewSet(viewsets.ModelViewSet):
 
     queryset = Patient.objects.all()
 
     serializer_class = PatientSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+
+        return Patient.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+
+        serializer.save(user=self.request.user)
